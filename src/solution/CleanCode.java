896a3dc2ad7b0cb4e02d9199b693cd5788f040cd
kag728 <42548585+kag728@users.noreply.github.com>
2019-02-28 16:12:56 -0600

    Update CleanCode.java

--- src/solution/CleanCode.java
+++ src/solution/CleanCode.java
@@ -103,11 +103,11 @@ public class CleanCode {
 	// fixed if statement to be less convoluted
 	public static boolean findEvens(int[] x) {
 		for(int i = 0; i < x.length; i++) {
-			if(x[i] % 2 == 0) {
-				return true;
+			if(x[i] % 2 != 0) {
+				return false;
 			}
 		}
-		return false;
+		return true;
 	}
 	
 	//changed method name - shouldn't use underscores
